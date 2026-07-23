d6cd5d3c6d5adc43508a38fa61b3f62a1484e5e5
connosssss <connorgriffin2424@gmail.com>
2026-07-23 15:22:56 -0700

    basic bootstrap

---
+++ src/boot.asm
@@ -0,0 +1,40 @@
+MBALIGN equ 1<<0
+MEMINFO equ 1<<1
+FLAGS   equ MBALGIN | MEMINFO
+MAGIC   equ 0x1BADB002         ; Number grub looks for
+CHECKSUM  equ -(MAGIC + FLAGS)
+
+
+
+section .multiboot
+aligh 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+
+; Building a useable stack in .bss
+section .bss
+align 16
+stack_bottom:
+    resb 16384    ; 16 kb kernel stack 
+stack_top:
+
+section .note.GNU-stack noalloc noexec nowrite progbits
+
+
+
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    mov esp, stack_top
+
+    call kernel_main
+    cli
+
+.hang:
+    hlt
+    jmp .hang
